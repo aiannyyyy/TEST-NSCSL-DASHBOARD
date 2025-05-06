8e0d306123fe08bcdb8155e4f68a20a8aadc6308
John Adrian Ticatic <john.ticatic@gmail.com>
2025-05-06 14:06:39 +0800

    todo list on the top of the page

--- js/reagentSupplies.js
+++ js/reagentSupplies.js
@@ -3,24 +3,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const REAGENT_DEFAULT_WARNING_TRESHOLD = 20;
 
     const itemThresholds = {
-        'REA005': {critical: 10, warning: 12, unit:'kits'},
+        'REA005': {critical: 5, warning: 7, unit:'boxes'},
         'REA006': {critical: 10, warning: 12, unit:'kits'},
         'REA007': {critical: 10, warning: 12, unit:'kits'},
         'REA008': {critical: 5, warning: 7, unit:'boxes'},
-        'REA019': {critical: 5, warning: 7, unit:'boxes'},
-        'REA025': {critical: 10, warning: 12, unit:'kits'},
-        'REA028': {critical: 2, warning: 4, unit:'kits'},
-        'REA029': {critical: 1, warning: 2, unit:'box'},
-        'REA030': {critical: 1, warning: 2, unit:'box'},
-        'REA031': {critical: 0.25, warning: 0.50, unit:'bottle'},
+        'REA019': {critical: 10, warning: 12, unit:'kits'},
+        'REA025': {critical: 2, warning: 4, unit:'bottles'},
+        'REA028': {critical: 15, warning: 17, unit:'kits'},
+        'REA029': {critical: 10, warning: 12, unit:'kits'},
+        'REA030': {critical: 15, warning: 17, unit:'kits'},
+        'REA031': {critical: 15, warning: 17, unit:'kits'},
         'REA032': {critical: 10, warning: 12, unit:'kits'},
         'REA033': {critical: 15, warning: 17, unit:'kits'},
-        'REA036': {critical: 10, warning: 12, unit:'kits'},
-        'REA051': {critical: 2 , warning: 4, unit:'bottles'},
-        'REA052': {critical: 15, warning: 17, unit:'kits'},
-        'REA053': {critical: 15, warning: 17, unit:'kits'},
-        'REA054': {critical: 15, warning: 17, unit:'kits'},
-        'REA057': {critical: 2, warning: 4, unit:'bottles'},
+        'REA036': {critical: 2, warning: 4, unit:'bottles'},
+        'REA051': {critical: 1 , warning: 2, unit:'box'},
+        'REA052': {critical: 1, warning: 2, unit:'box'},
+        'REA053': {critical: 10, warning: 12, unit:'kits'},
+        'REA054': {critical: 10, warning: 12, unit:'kits'},
+        'REA057': {critical: 0.25, warning: 0.50, unit:'bottle'},
     }
 
     function fetchReagentSupplies() {
